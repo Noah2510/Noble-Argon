3937412e11c5b8409b62102e620f718bd98ca91c
folso2 <126811009+folso2@users.noreply.github.com>
2023-03-13 23:24:08 -0600

    Update Canyon_statemachine.c
    I changed that state machine so it includes the interupts

--- Robot Code.X/Canyon_statemachine.c
+++ Robot Code.X/Canyon_statemachine.c
@@ -1,19 +1,26 @@
 #include "xc.h"
 #include "MotionFunctions.h"
 #pragma config FNOSC = FRCDIV // 8 MHz w post scaler
-
+#define right_wall_detect _LATB14//Pin 17 RB14 Right wall/service detection 
+#define center_wall_detect _LATB2//Pin 6 RB2 Center wall detection
 
 //2-30 cm CCW lowers detection distance; CW increases distance 2-30 cm
 //High= no obstacle 
 //Low= obstacle detected within range
 
-#define right_wall_detect _LATB14//Pin 17 RB14 Right wall/service detection 
-#define center_wall_detect _LATB2//Pin 6 RB2 Center wall detection
+unsigned static int steps = 0;
+
 unsigned static int PHASE_DETECTION=4;// global variable that counts phase strips on right side
 
 
+void __attribute__((interrupt, no_auto_psv))_OC2Interrupt(void) 
+{   
+    steps = steps + 1;
+    _OC2IF = 0; // clear flag
+} 
 
-enum {STRAIGHT,TURN} state;
+
+enum {STRAIGHT,TURNING} state;
 
 
 int main(void)
@@ -32,24 +39,33 @@ int main(void)
                     if(center_wall_detect==0)
                     {
                         STOP();
-                        state=TURN;
+                        if(right_wall_detect==0)
+                        {
+                            steps=0;
+                            Turn_Left();
+                            state=L_TURN;
+                        }
+                        if (right_wall_detect==1)
+                        {
+                            steps=0;
+                            Turn_Right();
+                            state=R_TURN;
+                        }
                     }
+                  
                     break;
-                case TURN:
-                    if(right_wall_detect==0)
-                    {
-                        Turn_Left();
-                        Forward();
-                        state=STRAIGHT;
-                    }
-                    else if (right_wall_detect==1)
-                    {
-                        Turn_Right();
-                        Forward();
-                        state=STRAIGHT;
-                    }
                     
-               
+                case TURNING:
+                    if (steps==50)
+                    {
+                        steps=0;
+                        Forward();
+                        state=STRAIGHT;
+                    }
+                 
+                    break;
+                    
+                
                         
             }
                         
@@ -74,6 +90,4 @@ int main(void)
     
     
     
-
-\
     
