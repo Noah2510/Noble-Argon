fb0277c02046c4f42cebddf3ee14c62164bd3eee
Noah2510 <126749354+Noah2510@users.noreply.github.com>
2023-03-21 12:45:18 -0600

    Updated Brain 3/21

--- Robot Code.X/Robot_Brain.c
+++ Robot Code.X/Robot_Brain.c
@@ -7,6 +7,7 @@
 
 #include "xc.h"
 #include "MotionFunctions.h"
+#include "Task_Functions.h"
 #pragma config FNOSC = FRCDIV // 8 MHz w post scaler
 
 // PWM interrupt
@@ -21,13 +22,28 @@ void __attribute__((interrupt, no_auto_psv)) _INT0Interrupt(void)
 {
     _INT0IF = 0; // clear flag
     task_counter++;
+    
+    if (task_counter == 1){
+        _LATA2 = 1;
+        TMR1 = 0;
+        T1CONbits.TON = 1; // turn on timer for line counting
+//        TMR2 = 0;
+//        T2CONbits.TON = 1; // turn on timer for spot avoidance
+    }
+//    else if (TMR2 <= 1000 && TMR2 >= 100)
+//    {
+//        task_counter--;
+//        TMR2 = 0;
+//    }
+
 }
-enum { STRAIGHT, ADJ_R, ADJ_L} state;
+enum { STRAIGHT, ADJ_R, ADJ_L, TASK_COUNT} state;
 
 int main(void) {
     
     Motion_Setup();
     INT_Setup();
+    Analog_Setup();
     Timer_Setup();
     Forward();
     //Turn_Left();
@@ -66,25 +82,10 @@ int main(void) {
 //                    state = STRAIGHT;
 //                }
                 
-                else if (task_QRD == 1 && task_counter < 1)
+                else if (TMR1 >= 20000)
                 {
-                    _TON = 1; // start timer
-                    task_counter = 0;
-                    _LATA2 = 1;
-                    //task_counter = Count_Tasks();
-                    //state = PHASE_DETECTED;
-                }
-                
-                else if (TMR1 >= 35000 && task_counter == 1)
-                {
-                    task_counter = 0;
-                    _TON = 0;
-                    TMR1 = 0;
-                }
-                
-                else if (TMR1 >= 35000 && task_counter == 2)
-                {
-                    STOP();
+                    _LATA2 = 0;
+                    state = TASK_COUNT;
                 }
                 
                 break;
@@ -107,25 +108,10 @@ int main(void) {
                     state = ADJ_R;
                 }
                 
-                else if (task_QRD == 1 && task_counter < 1)
+                else if (TMR1 >= 20000)
                 {
-                    _TON = 1; // start timer
-                    task_counter = 0;
-                    _LATA2 = 1;
-                    //task_counter = Count_Tasks();
-                    //state = PHASE_DETECTED;
-                }
-                
-                else if (TMR1 >= 35000 && task_counter == 1)
-                {
-                    task_counter = 0;
-                    _TON = 0;
-                    TMR1 = 0;
-                }
-                
-                else if (TMR1 >= 35000 && task_counter >= 2)
-                {
-                    STOP();
+                    _LATA2 = 0;
+                    state = TASK_COUNT;
                 }
                 
 //                else if (right_QRD_dig == 0 && left_QRD_dig == 0 && mid_QRD_dig == 0)
@@ -156,25 +142,10 @@ int main(void) {
                     state = ADJ_L;
                 }
                 
-                else if (task_QRD == 1 && task_counter < 1)
+                else if (TMR1 >= 20000)
                 {
-                    _TON = 1; // start timer
-                    task_counter = 0;
-                    _LATA2 = 1;
-                    //task_counter = Count_Tasks();
-                    //state = PHASE_DETECTED;
-                }
-                
-                else if (TMR1 >= 35000 && task_counter == 2)
-                {
-                    STOP();
-                }
-                
-                else if (TMR1 >= 46875 && task_counter == 1)
-                {
-                    task_counter = 0;
-                    _TON = 0;
-                    TMR1 = 0;
+                    _LATA2 = 0;
+                    state = TASK_COUNT;
                 }
                 
 //                else if (right_QRD_dig == 0 && left_QRD_dig == 0 && mid_QRD_dig == 0)
@@ -186,6 +157,35 @@ int main(void) {
 //                }
                 
                 break;
+                
+            case TASK_COUNT:
+                
+                if (task_counter < 3)
+                {
+                    Ball_Collection();
+                    task_counter = 0;
+                    T1CONbits.TON = 0;
+                    TMR1 = 0;
+                    state = ADJ_R;
+                }
+                
+                else if (task_counter == 3)
+                {
+                    STOP();
+                    task_counter = 0;
+                    T1CONbits.TON = 0;
+                    TMR1 = 0;
+                }
+                
+                else if (task_counter == 4)
+                {
+                    task_counter = 0;
+                    T1CONbits.TON = 0;
+                    TMR1 = 0;
+                    state = STRAIGHT;
+                    STOP();
+                }
+                    
         }
     }
     return 0;
